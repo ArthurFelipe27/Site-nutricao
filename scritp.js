25b18273c5c51c821d11a834f0c185077a8c151a
ArthurFelipe27 <arthurfelipematos837@gmail.com>
2025-05-03 23:07:33 -0300

    Update 0.7

--- scritp.js
+++ scritp.js
@@ -1,10 +1,46 @@
 const form = document.querySelector("form");
-  const botao = document.getElementById("btn-enviar");
-  const texto = document.getElementById("btn-texto");
-  const spinner = document.getElementById("spinner");
+const botao = document.getElementById("btn-enviar");
+const texto = document.getElementById("btn-texto");
+const spinner = document.getElementById("spinner");
 
-  form.addEventListener("submit", () => {
-    botao.disabled = true;
-    texto.textContent = "Enviando...";
-    spinner.style.display = "inline-block";
-  });
+form.addEventListener("submit", () => {
+  botao.disabled = true;
+  texto.textContent = "Enviando...";
+  spinner.style.display = "inline-block";
+});
+
+
+
+
+const formIMC = document.getElementById('formIMC');
+const resultado = document.getElementById('resultadoIMC');
+
+formIMC.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const peso = parseFloat(document.getElementById('peso').value);
+  const altura = parseFloat(document.getElementById('altura').value);
+
+  if (peso > 0 && altura > 0) {
+    const imc = peso / (altura * altura);
+    let classificacao = '';
+
+    if (imc < 18.5) {
+      classificacao = 'Abaixo do peso';
+    } else if (imc < 24.9) {
+      classificacao = 'Peso normal';
+    } else if (imc < 29.9) {
+      classificacao = 'Sobrepeso';
+    } else if (imc < 34.9) {
+      classificacao = 'Obesidade grau 1';
+    } else if (imc < 39.9) {
+      classificacao = 'Obesidade grau 2';
+    } else {
+      classificacao = 'Obesidade grau 3 (mórbida)';
+    }
+
+    resultado.innerHTML = `Seu IMC é <strong>${imc.toFixed(2)}</strong><br>${classificacao}`;
+  } else {
+    resultado.innerHTML = 'Por favor, insira valores válidos.';
+  }
+});
